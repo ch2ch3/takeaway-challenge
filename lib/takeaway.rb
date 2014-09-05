878bfdacc03f67e36c98d8e52004ea731e3e5095
Ethel Ng <ethel.ng@gmail.com>
2014-09-05 17:22:19 +0100

    All tests passing

---
+++ lib/takeaway.rb
@@ -0,0 +1,73 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'tokens'
+
+class Takeaway
+
+	attr_reader :client, :total_price
+
+	MENU = {
+		:popiah => 2,
+		:sambal_kang_kong => 3,
+		:hokkien_mee => 4,
+		:char_kway_teow => 4,
+		:century_egg_tofu => 4,
+		:sliced_fish_soup => 5,
+		:laksa => 5,
+		:black_pepper_crab => 20
+	}
+
+	def show_menu
+		puts "#  DISH".ljust(28) + "PRICE".rjust(8)
+		MENU.each do |dish, price|
+			puts "#{MENU.keys.index(dish)+1}.".ljust(3) + "#{dish.to_s.split("_").map(&:capitalize).join(" ")}".ljust(25) + "$#{'%.2f' % price}".rjust(8)
+		end
+	end
+
+	def order(dish_number)
+		buffer[MENU.keys[dish_number-1]] += 1
+	end
+
+	def buffer
+		@buffer ||= Hash.new(0)
+	end
+
+	def show_current_order
+		buffer.sort_by { |dish, quantity| quantity }
+		print_order
+		print_total_price
+	end
+
+	def print_order
+		puts "YOUR ORDER".center(36)
+		puts "#  DISH".ljust(28) + "QUANTITY".rjust(8)
+		buffer.each do |dish, quantity|
+			puts "#{buffer.keys.index(dish)+1}.".ljust(3) + "#{dish.to_s.split("_").map(&:capitalize).join(" ")}".ljust(25) + "#{quantity}".rjust(8)
+		end
+	end
+
+	def total_price
+		total_price = 0
+		buffer.each do |dish, quantity|
+			total_price += (MENU[dish] * quantity)
+		end
+		total_price
+	end
+
+	def print_total_price
+		puts "\nTotal price: #{'%.2f' % total_price}".ljust(36)
+	end
+
+	def place_order
+		account_sid = Tokens::ACCOUNT_SID 
+		auth_token = Tokens::AUTH_TOKEN
+		delivery_time = Time.now + 3600
+		client = Twilio::REST::Client.new account_sid, auth_token
+		client.account.messages.create({
+			:from => '+441994342030', 
+			:to => '+447858009017',
+			:body => "Excellent choice of food! Your order will be with you by #{delivery_time.hour}:#{'%02d' %delivery_time.min}."
+		})
+	end
+
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,34 @@
+require 'takeaway'
+
+describe Takeaway do
+
+	let(:takeaway) { Takeaway.new }
+
+	it "can list dishes and their prices" do
+		expect(takeaway.respond_to?(:show_menu)).to be true
+	end
+
+	it "lets the customer select the dish to be ordered" do
+		takeaway.order(1)
+		expect(takeaway.buffer).to eq ({:popiah => 1})
+	end
+
+	it "keeps a list of ordered dishes and their quantities" do
+		takeaway.order(1)
+		takeaway.order(4)
+		takeaway.order(1)
+		expect(takeaway.buffer.size).to eq 2
+	end
+
+	it "can calculate the total price of the order" do
+		takeaway.order(1)
+		takeaway.order(8)
+		expect(takeaway.total_price).to eq 22
+	end
+
+	it "places the order" do
+		expect(takeaway).to receive(:place_order).and_return("Dummy text.")
+		takeaway.place_order
+	end
+
+end
